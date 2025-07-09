d4cd0763a24ee5c542956b37526e87587d2cc227
AnnyCherkashina <149014624+AnnyCherkashina@users.noreply.github.com>
2025-07-09 14:14:32 +0300

    update

--- src/server.js
+++ src/server.js
@@ -11,14 +11,22 @@ import errorHandler from './middlewares/errorHandler.js';
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(pino());
 app.use(express.json());
 app.use(cookieParser());
 
+// Routes
 app.use('/contacts', contactsRouter);
 app.use('/auth', authRouter);
 
+// Base route (для перевірки, що API працює)
+app.get('/', (req, res) => {
+    res.json({ message: 'API is running. Welcome to HW6!' });
+});
+
+// Error handling
 app.use(notFoundHandler);
 app.use(errorHandler);
 
